d4a4cc58e07e559a68c0ed05c12fddb228f9217e
Felipe Versiane <felipeversiane09@gmail.com>
2023-03-19 17:47:23 -0300

    :pencil: edited version

--- index.py
+++ index.py
@@ -1,8 +1,20 @@
 import wsgiref.simple_server
 import urllib.parse
 import os
+import json
 
 root = os.path.dirname(os.path.abspath(__file__))
+
+def render_feedbacks(feedbacks):
+    html = "<h1>Feedbacks:</h1>\n<ul>\n"
+    for feedback in feedbacks:
+        html += "<li>Name: {}<br>Email: {}<br>Feedback: {}</li>\n".format(
+            feedback["name"],
+            feedback["email"],
+            feedback["feedback"]
+        )
+    html += "</ul>"
+    return html
 
 def app(environ, start_response):
     path = environ["PATH_INFO"]
@@ -10,13 +22,18 @@ def app(environ, start_response):
     data=b""
     forms_data = []
 
-
+    if path == "/forms":
+        feedbacks = []
+        with open(os.path.join(root, "feedbacks.json"), "r") as f:
+            feedbacks = json.load(f)
+        html = render_feedbacks(feedbacks)
+        data = html.encode("utf-8")
     if path == "/":  
         with open(os.path.join(root, "home/home.html"),"rb") as f:
             data=f.read()
     if path == "/formsucess":
         with open(os.path.join(root, "form/form-sucessfully.html"), "rb") as f:
-                data = f.read()
+            data = f.read()
     if path == "/feedback":
         if method == "POST":
             input_obj = environ["wsgi.input"]
@@ -26,16 +43,24 @@ def app(environ, start_response):
             req = { 
                 "name" : params.get('name', [''])[0],
                 "email" : params.get('email', [''])[0],
-                "message" : params.get('message', [''])[0]
+                "feedback" : params.get('feedback', [''])[0]
             }
-            forms_data.append(req)
+            feedbacks = []
+            with open(os.path.join(root, "feedbacks.json"), "r") as f:
+                feedbacks = json.load(f)
+            feedbacks.append(req)
+            with open(os.path.join(root, "feedbacks.json"), "w") as f:
+                json.dump(feedbacks, f, indent=4)
+            start_response("302 Found", [
+                ("Location", "/formsucess"),
+            ])
+            return iter([])
         if method == "GET":
             with open(os.path.join(root, "form/form.html"), "rb") as f:
                 data = f.read()
-    elif path != "/" and path != "/feedback" and path!= "/formsucess":
+    elif path != "/" and path != "/feedback" and path!= "/formsucess" and path!= "/forms":
          with open(os.path.join(root, "error/error404.html"), "rb") as f:
             data = f.read()
-            
     start_response("200 OK", [
         ("Content-Type", "text/html; charset=utf-8"),
         ("Content-Length", str(len(data)))
